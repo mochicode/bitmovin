deaf9f38839e628d23644d82b45597f4237359aa
Michael Riezler <michael@riezler.co>
2022-04-15 11:09:53 +0200

    initial commit

---
+++ encoding-api/src/main/java/com/bitmovin/trial/encodingapi/EncodingApiApplication.java
@@ -0,0 +1,13 @@
+package com.bitmovin.trial.encodingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncodingApiApplication.class, args);
+	}
+
+}
